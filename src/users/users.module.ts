1ab84155f689cf4ba9cb4e124a3c05a8b36c1ace
Nguyen Thanh Minh <thanhminh339968az@gmail.com>
2023-03-31 17:44:07 +0700

    Add Bai-12

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,36 @@
-import { Module } from '@nestjs/common';
-import { UserController } from './user.controller';
-import { UserService } from './user.service';
+import { Module } from "@nestjs/common";
+import { StoreConfig } from "src/store/store.config";
+import { StoreService } from "./store.service";
+import { UserController } from "./users.controller";
+import { UserService } from "./users.service";
+function createStore(config: StoreConfig): StoreService {
+  console.log(config);
+  return new StoreService();
+}
 
 @Module({
-  controllers: [UserController],
-  providers: [
+  controllers:[UserController],
+  providers:[
+    UserService,
     {
-      provide: 'USER_SERVICE_VU',
-      useClass: UserService,
+      provide: 'STORE_CONFIG',
+      useValue: {
+        dir: 'store',
+        path: 'user',
+
+      } as StoreConfig,
     },
+    {
+      provide: 'STORE_SERVICE',
+      useFactory: createStore,
+      inject: [
+        {
+          token: 'STORE_CONFIG',
+          optional: true,
+        }
+      ]
+    }
   ],
 })
-export class UserModule {}
+
+export class UsersModule {}
